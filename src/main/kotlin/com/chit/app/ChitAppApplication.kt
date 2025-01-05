f3891607eaceb3a5d8229b394c7efcabb918f41d
Myoungsung Ko <msko@bigxdata.io>
2025-01-05 23:48:37 +0900

    🔧 config: JwtFilterProperties 설정 활성화 어노테이션 추가
    변경 내용:
    
    - `@EnableConfigurationProperties` 어노테이션을 추가하여 `JwtFilterProperties` 설정 클래스 활성화

--- src/main/kotlin/com/chit/app/ChitAppApplication.kt
+++ src/main/kotlin/com/chit/app/ChitAppApplication.kt
@@ -1,9 +1,12 @@
 package com.chit.app
 
+import com.chit.app.domain.auth.infrastructure.properties.JwtFilterProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableConfigurationProperties(JwtFilterProperties::class)
 @EnableJpaAuditing
 @SpringBootApplication
 class ChitAppApplication
