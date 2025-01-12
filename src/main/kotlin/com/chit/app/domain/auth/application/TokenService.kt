1de6080335a9611355acec588b3fe9c3ffdb984b
Myoungsung Ko <msko@bigxdata.io>
2025-01-12 17:09:24 +0900

    ✨ feat: TokenService 클래스 생성
    변경 내용:
    
    - 주요 메서드:
      - `getRefreshToken(memberId: Long)`: `@Cacheable`을 사용하여 캐시에서 리프레시 토큰을 조회. 캐시에 없을 경우 `null` 반환.
      - `putRefreshToken(refreshToken: RefreshToken)`: `@CachePut`을 사용하여 주어진 리프레시 토큰을 캐시에 저장 및 갱신.
      - `deleteRefreshToken(refreshToken: RefreshToken)`: `@CacheEvict`을 사용하여 해당 회원의 리프레시 토큰을 캐시에서 제거.

---
+++ src/main/kotlin/com/chit/app/domain/auth/application/TokenService.kt
@@ -0,0 +1,30 @@
+package com.chit.app.domain.auth.application
+
+import com.chit.app.domain.auth.domain.model.RefreshToken
+import com.chit.app.global.delegate.logger
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.CachePut
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+
+@Service
+class TokenService {
+    
+    private val log = logger<TokenService>()
+    
+    @Cacheable(value = ["refreshTokenCache"], key = "#memberId")
+    fun getRefreshToken(memberId: Long): RefreshToken? {
+        return null
+    }
+    
+    @CachePut(value = ["refreshTokenCache"], key = "#refreshToken.memberId")
+    fun putRefreshToken(refreshToken: RefreshToken): RefreshToken {
+        return refreshToken
+    }
+    
+    @CacheEvict(value = ["refreshTokenCache"], key = "#refreshToken.memberId")
+    fun deleteRefreshToken(refreshToken: RefreshToken) {
+        log.info("회원 ID: ${refreshToken.memberId} 에 대한 리프레시 토큰이 캐시에서 삭제되었습니다.")
+    }
+    
+}
