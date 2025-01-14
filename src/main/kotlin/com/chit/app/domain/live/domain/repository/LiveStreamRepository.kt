e7c90a12bdb409b6efa20f7e9508327b24e9ed46
Myoungsung Ko <msko@bigxdata.io>
2025-01-15 00:02:38 +0900

    ♻️ refactor: LiveStream 저장 기능 추가 및 예외 처리 적용
    변경 내용:
    
    - `save` 메서드 추가: `LiveStream` 엔티티를 저장하는 기능 구현
      - `EntitySaveExceptionHandler`를 사용하여 저장 시 발생할 수 있는 예외 처리

--- src/main/kotlin/com/chit/app/domain/live/domain/repository/LiveStreamRepository.kt
+++ src/main/kotlin/com/chit/app/domain/live/domain/repository/LiveStreamRepository.kt
@@ -3,12 +3,19 @@ package com.chit.app.domain.live.domain.repository
 import com.chit.app.domain.live.domain.model.LiveStream
 import com.chit.app.domain.live.domain.model.LiveStatus
 import com.chit.app.domain.live.infrastructure.JpaLiveStreamRepository
+import com.chit.app.global.handler.EntitySaveExceptionHandler
 import org.springframework.stereotype.Repository
 
 @Repository
 class LiveStreamRepository(
         private val repository: JpaLiveStreamRepository
 ) {
+    
+    fun save(liveStream: LiveStream): LiveStream =
+            runCatching { repository.save(liveStream) }
+                    .onFailure { EntitySaveExceptionHandler.handle(it) }
+                    .getOrThrow()
+    
     
     fun findOpenLiveStreamByStreamerId(streamerId: Long): LiveStream? =
             repository.findByStreamerIdAndLiveStatus(streamerId, LiveStatus.OPEN)
