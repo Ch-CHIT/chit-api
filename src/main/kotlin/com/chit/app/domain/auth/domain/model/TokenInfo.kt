662b6fe67adcf8a949b71ef10058917347a5c20b
Myoungsung Ko <msko@bigxdata.io>
2025-01-14 23:26:17 +0900

    ✨ feat: TokenInfo에 채널 및 사용자 정보 필드 추가

--- src/main/kotlin/com/chit/app/domain/auth/domain/model/TokenInfo.kt
+++ src/main/kotlin/com/chit/app/domain/auth/domain/model/TokenInfo.kt
@@ -2,5 +2,7 @@ package com.chit.app.domain.auth.domain.model
 
 data class TokenInfo(
         val accessToken: String,
-        val refreshToken: String
+        val refreshToken: String,
+        val channelId: String,
+        val memberId: Long?
 )
