31616c88dfc98630d20f0702e4e24c95ebcc7b9e
Myoungsung Ko <msko@bigxdata.io>
2025-01-14 23:59:55 +0900

    ✨ feat: AuthController 클래스 추가
    변경 내용:
    
    - `/api/v1/auth/login` 엔드포인트 추가: 사용자 로그인 요청을 처리하여 액세스 토큰 발급 및 리프레시 토큰 쿠키 저장
    - `/api/v1/auth/logout` 엔드포인트 추가: 사용자 로그아웃 처리 및 리프레시 토큰 쿠키 삭제
    - `CookieManager`를 통해 쿠키 추가 및 삭제 처리
    - `LiveStreamService.upsert` 호출을 통해 사용자 세션 정보 갱신

---
+++ src/main/kotlin/com/chit/app/domain/auth/presentation/AuthController.kt
@@ -0,0 +1,57 @@
+package com.chit.app.domain.auth.presentation
+
+import com.chit.app.domain.auth.application.AuthService
+import com.chit.app.domain.auth.presentation.annotation.CurrentMemberId
+import com.chit.app.domain.auth.presentation.dto.LoginRequestDto
+import com.chit.app.domain.live.application.LiveStreamService
+import com.chit.app.global.delegate.Message
+import com.chit.app.global.delegate.Void
+import com.chit.app.global.response.SuccessResponse.Companion.success
+import com.chit.app.global.response.SuccessResponse.Companion.successWithData
+import com.chit.app.global.util.CookieInfo
+import com.chit.app.global.util.CookieManager
+import io.swagger.v3.oas.annotations.Parameter
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthController(
+        private val cookieManager: CookieManager,
+        private val authService: AuthService,
+        private val liveStreamService: LiveStreamService
+) {
+    
+    @PostMapping("/login")
+    fun login(
+            @RequestBody @Valid request: LoginRequestDto,
+            httpResponse: HttpServletResponse
+            
+    ): Message {
+        val tokenInfo = authService.login(code = request.code, state = request.state)
+        cookieManager.addCookie(
+            response = httpResponse,
+            cookieInfo = CookieInfo.REFRESH_TOKEN,
+            value = tokenInfo.refreshToken
+        )
+        liveStreamService.upsert(tokenInfo.memberId, tokenInfo.channelId)
+        return successWithData(tokenInfo.accessToken)
+    }
+    
+    @PostMapping("/logout")
+    fun logout(
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            @Parameter(hidden = true) @CurrentMemberId currentMemberId: Long
+    ): Void {
+        cookieManager.deleteCookie(request, response, CookieInfo.REFRESH_TOKEN)
+        authService.logout(currentMemberId)
+        return success()
+    }
+    
+}
