dbd6f19d6506fb09229be54efa1be4b387309675
Myoungsung Ko <msko@bigxdata.io>
2025-01-19 16:49:33 +0900

    feat: EnableScheduling 어노테이션 추가로 스케줄링 기능 활성화
    Changes:
    - @EnableScheduling 어노테이션 추가하여 스케줄링 작업 지원
    
    Context:
    - 배치 작업이나 주기적인 프로세스를 처리하기 위해 스케줄링 기능 활성화 필요

--- src/main/kotlin/com/chit/app/ChitAppApplication.kt
+++ src/main/kotlin/com/chit/app/ChitAppApplication.kt
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableConfigurationProperties(JwtFilterProperties::class)
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 class ChitAppApplication
