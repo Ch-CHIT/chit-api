5fc3354417cfc37ded76fc973b7381eb65610eed
Myoungsung Ko <msko@bigxdata.io>
2025-01-26 20:50:24 +0900

    refactor: LiveStreamRepository 리팩터링 및 Querydsl 기반 쿼리 처리 적용
    Changes:
    - 기존 `JpaLiveStreamRepository` 의존성을 중심으로 구현된 메서드들을 Querydsl 기반으로 리팩터링
      - 통합 메서드 `findOpenLiveStreamBy` 추가
        - 스트리머 ID와 채널 ID를 동적으로 조회 가능
        - `findOpenLiveStreamByStreamerId` 및 `findOpenLiveStreamByChannelId` 제거
      - `findAllOpenLiveStreams`: Querydsl로 구현하여 동적 쿼리 처리
    
    Context:
    - Querydsl 도입으로 동적 쿼리 작성을 유연하게 처리
    - 기존 중복된 메서드를 통합하여 코드 가독성 및 유지보수성 향상
    - 동적 조건 기반으로 스트리머 ID와 채널 ID에 따라 열린 라이브 스트림 조회 가능

--- src/main/kotlin/com/chit/app/domain/live/domain/repository/LiveStreamRepository.kt
+++ src/main/kotlin/com/chit/app/domain/live/domain/repository/LiveStreamRepository.kt
@@ -1,30 +1,43 @@
 package com.chit.app.domain.live.domain.repository
 
+import com.chit.app.domain.live.domain.model.LiveStatus
 import com.chit.app.domain.live.domain.model.LiveStream
+import com.chit.app.domain.live.domain.model.QLiveStream
 import com.chit.app.domain.live.infrastructure.JpaLiveStreamRepository
 import com.chit.app.global.handler.EntitySaveExceptionHandler
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 
 @Repository
 class LiveStreamRepository(
+        private val query: JPAQueryFactory,
         private val repository: JpaLiveStreamRepository
 ) {
+    
+    private val liveStream: QLiveStream = QLiveStream.liveStream
     
     fun save(liveStream: LiveStream): LiveStream =
             runCatching { repository.save(liveStream) }
                     .onFailure { EntitySaveExceptionHandler.handle(it) }
                     .getOrThrow()
     
-    fun findOpenLiveStreamByStreamerId(streamerId: Long): LiveStream? =
-            repository.findByStreamerIdAndLiveStatus(streamerId)
+    fun batchUpdateLiveStreams(liveStreams: List<LiveStream>) = repository.saveAll(liveStreams)
     
-    fun findOpenLiveStreamByChannelId(channelId: String): LiveStream? =
-            repository.findByChannelIdAndLiveStatus(channelId)
+    fun findOpenLiveStreamBy(
+            streamerId: Long? = null,
+            channelId: String? = null,
+    ): LiveStream? = query
+            .selectFrom(liveStream)
+            .where(
+                streamerId?.let { liveStream.streamerId.eq(it) },
+                channelId?.let { liveStream.channelId.eq(it) },
+                liveStream._liveStatus.eq(LiveStatus.OPEN)
+            )
+            .fetchOne()
     
-    fun findAllOpenLiveStreams(): List<LiveStream> =
-            repository.findAllByLiveStatus()
-    
-    fun batchUpdateLiveStreams(liveStreams: List<LiveStream>) =
-            repository.saveAll(liveStreams)
+    fun findAllOpenLiveStreams(): List<LiveStream> = query
+            .selectFrom(liveStream)
+            .where(liveStream._liveStatus.eq(LiveStatus.OPEN))
+            .fetch()
     
 }
