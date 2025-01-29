7a1a1377f9c5d302316b583a3b31310be1e6fceb
Myoungsung Ko <msko@bigxdata.io>
2025-01-29 23:24:34 +0900

    dependency: Redis, 캐시, Flyway 관련 의존성 추가
    Changes:
    - `spring-boot-starter-data-redis` 추가하여 Redis 연동 지원
    - `ehcache:3.10.8` 및 `hibernate-jcache:6.5.3.Final` 추가하여 JPA 캐싱 기능 활성화
    - `flyway-database-postgresql:10.20.1` 추가하여 PostgreSQL 기반 마이그레이션 지원
    
    Context:
    - Redis를 활용한 세션 관리 및 캐싱 기능을 도입하기 위해 의존성 추가
    - Hibernate 2차 캐시를 적용하기 위해 Ehcache 및 JCache 지원 추가
    - Flyway를 사용한 DB 마이그레이션 관리 필요성에 따라 PostgreSQL 용 모듈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     
     // JSON 직렬화 및 역직렬화
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -41,9 +42,14 @@ dependencies {
     kapt("jakarta.persistence:jakarta.persistence-api")
     
     // 캐시 및 저장소 라이브러리
-    implementation("org.ehcache:ehcache:3.10.8:jakarta")
+    implementation("org.ehcache:ehcache:3.10.8")
+    implementation("org.hibernate.orm:hibernate-jcache:6.5.3.Final")
+    
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
+    
+    // flyway
+    implementation("org.flywaydb:flyway-database-postgresql:10.20.1")
     
     // 보안 관련 라이브러리
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
