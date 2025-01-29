de6f8994c9b6d7cee2ecba30faec14b4c6ed6572
Myoungsung Ko <msko@bigxdata.io>
2025-01-29 23:19:30 +0900

    remove: Querydsl 사용으로 모든 쿼리 메서드 삭제
    Changes:
    - `findSortedParticipantsBySessionCode` 메서드 삭제
    
    Context:
    - 기존 JPQL 기반의 `findSortedParticipantsBySessionCode`는 Querydsl로 대체됨

--- src/main/kotlin/com/chit/app/domain/session/infrastructure/JpaSessionParticipantRepository.kt
+++ src/main/kotlin/com/chit/app/domain/session/infrastructure/JpaSessionParticipantRepository.kt
@@ -1,28 +1,6 @@
 package com.chit.app.domain.session.infrastructure
 
 import com.chit.app.domain.session.domain.model.SessionParticipant
-import com.chit.app.domain.session.domain.model.status.ParticipationStatus
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
-interface JpaSessionParticipantRepository : JpaRepository<SessionParticipant, Long> {
-    
-    @Query(
-        """
-        SELECT sp
-        FROM SessionParticipant sp
-          JOIN FETCH sp.contentsSession cs
-        WHERE cs.sessionCode = :code
-          AND sp._status != :status
-        ORDER BY sp._fixedPick DESC,
-                 sp._status ASC,
-                 sp.id ASC
-        """
-    )
-    fun findSortedParticipantsBySessionCode(
-            @Param("code") sessionCode: String,
-            @Param("status") status: ParticipationStatus
-    ): List<SessionParticipant>
-    
-}
+interface JpaSessionParticipantRepository : JpaRepository<SessionParticipant, Long>
