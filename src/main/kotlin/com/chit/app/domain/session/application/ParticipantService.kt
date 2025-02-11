127efa94f4fd55bd6072648462b4984a6467fa7a
Myoungsung Ko <msko@bigxdata.io>
2025-02-12 00:17:59 +0900

    refactor: joinSession 메서드를 suspend 함수로 변경
    Changes:
    - StreamerSseService의 suspend 함수 호출을 위해 비동기 실행 가능하도록 수정

--- src/main/kotlin/com/chit/app/domain/session/application/ParticipantService.kt
+++ src/main/kotlin/com/chit/app/domain/session/application/ParticipantService.kt
@@ -18,14 +18,15 @@ class ParticipantService(
     private val log = logger<ParticipantService>()
     
     @Transactional
-    fun joinSession(sessionCode: String, participantId: Long, gameNickname: String) {
-        // 세션 코드에 해당하는 열린 세션을 조회
+    suspend fun joinSession(
+            sessionCode: String,
+            participantId: Long,
+            gameNickname: String
+    ) {
         val contentsSession = sessionRepository.findOpenContentsSessionBy(sessionCode = sessionCode) ?: run {
             throw IllegalArgumentException("입력하신 세션 참여 코드를 가진 세션을 찾을 수 없습니다. 다시 확인해 주세요.")
         }
-        log.debug("세션 조회 성공 - sessionCode: $sessionCode, streamerId: ${contentsSession.streamerId}")
         
-        // 새로운 참가자 생성 및 세션에 추가
         sessionRepository.addParticipant(
             SessionParticipant.create(
                 participantId = participantId,
@@ -33,9 +34,7 @@ class ParticipantService(
                 contentsSession = contentsSession
             )
         )
-        log.debug("참가자 추가 성공 - participantId: $participantId, gameNickname: $gameNickname, sessionCode: $sessionCode")
         
-        // SSE 이벤트 발행
         streamerSseService.publishEvent(
             streamerId = contentsSession.streamerId,
             event = SseEvent.PARTICIPANT_ADDED,
