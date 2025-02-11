5e2885b3183d119410dbf91dc8f180cd5aed065d
Myoungsung Ko <msko@bigxdata.io>
2025-02-11 22:56:45 +0900

    feat: @EnableAsync 추가로 비동기 작업 지원

--- src/main/kotlin/com/chit/app/ChitAppApplication.kt
+++ src/main/kotlin/com/chit/app/ChitAppApplication.kt
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableConfigurationProperties(JwtFilterProperties::class)
 @EnableScheduling
 @EnableJpaAuditing
+@EnableAsync
 @SpringBootApplication
 class ChitAppApplication
 
