cfe53519b4b19e118614e30f204c771bb05b6a57
Myoungsung Ko <msko@bigxdata.io>
2025-02-18 12:10:26 +0900

    dependency: `build.gradle.kts` 정리 및 `jaxb` 라이브러리 추가
    Changes:
    - `javax.xml.bind:jaxb-api` 및 `com.sun.xml.bind:jaxb-impl` 라이브러리 추가
    - `dependencies` 정리 및 그룹화하여 가독성 향상
    - 중복되는 주석 및 불필요한 코드 정리
    
    Context:
    - `jaxb-api` 및 `jaxb-impl` 추가를 통해 XML 바인딩 기능 지원 강화

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,9 @@ dependencies {
     // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     
-    // JSON 직렬화 및 역직렬화
+    // JSON 처리 (직렬화/역직렬화)
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     
     // Kotlin 관련 라이브러리
@@ -37,33 +34,40 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     
-    // Querydsl
+    // 데이터 액세스 및 캐시
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.ehcache:ehcache:3.10.8")
+    implementation("org.hibernate.orm:hibernate-jcache:6.5.3.Final")
+    
+    // QueryDSL
     implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
     kapt("jakarta.annotation:jakarta.annotation-api")
     kapt("jakarta.persistence:jakarta.persistence-api")
     
-    // 캐시 및 저장소 라이브러리
-    implementation("org.ehcache:ehcache:3.10.8")
-    implementation("org.hibernate.orm:hibernate-jcache:6.5.3.Final")
-    
+    // 데이터베이스
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     
-    // flyway
+    // DB 마이그레이션
     implementation("org.flywaydb:flyway-database-postgresql:10.20.1")
     
-    // 보안 관련 라이브러리
+    // JWT 및 보안 관련
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
     
-    // 유틸리티 라이브러리
+    // 유틸리티
     implementation("com.aventrix.jnanoid:jnanoid:2.0.0")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
     
     // API 문서화
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    implementation("com.sun.xml.bind:jaxb-impl:2.3.3")
     
     // 테스트 관련 라이브러리
     testImplementation("org.springframework.security:spring-security-test")
