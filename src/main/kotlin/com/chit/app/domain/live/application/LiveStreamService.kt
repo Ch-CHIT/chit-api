8bc4b9e44ce5e048fc9db00c18d6f91e9e2ad1f7
Myoungsung Ko <msko@bigxdata.io>
2025-02-21 00:37:33 +0900

    feat: 라이브 스트림 상태 동기화 기능 추가 및 로깅 개선
    Changes:
    - `syncLiveStreamStatus()` 메서드를 추가하여 외부 API를 기반으로 라이브 스트림 상태를 동기화하도록 구현
      -> `log.debug()`를 추가하여 라이브 스트림 종료 및 업데이트 로그를 기록
    
    Context:
    - 라이브 스트림 ID가 변경되었을 경우 기존 스트림을 종료 처리하고 새 스트림을 생성하여 일관성을 유지

--- src/main/kotlin/com/chit/app/domain/live/application/LiveStreamService.kt
+++ src/main/kotlin/com/chit/app/domain/live/application/LiveStreamService.kt
@@ -5,14 +5,18 @@ import com.chit.app.domain.live.domain.model.LiveStream
 import com.chit.app.domain.live.domain.repository.LiveStreamRepository
 import com.chit.app.domain.live.infrastructure.ChzzkLiveApiClient
 import com.chit.app.domain.live.infrastructure.response.LiveDetailResponse
+import com.chit.app.global.delegate.logger
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime.now
 
 @Service
 class LiveStreamService(
         private val chzzkLiveApiClient: ChzzkLiveApiClient,
         private val liveStreamRepository: LiveStreamRepository,
 ) {
+    
+    private val log = logger<LiveStreamService>()
     
     @Transactional
     fun saveOrUpdateLiveStream(streamerId: Long?, channelId: String) {
@@ -31,6 +35,24 @@ class LiveStreamService(
         } else {
             // 라이브 ID가 같으면 업데이트
             updateLiveStream(existingLiveStream, fetchedLiveDetail)
+        }
+    }
+    
+    @Transactional
+    fun syncLiveStreamStatus(liveStream: LiveStream): LiveStream? {
+        return chzzkLiveApiClient.fetchChzzkLiveDetail(liveStream.channelId!!)?.let { fetchLiveDetail ->
+            if (liveStream.liveId != fetchLiveDetail.liveId) {
+                liveStream.apply {
+                    liveStatus = LiveStatus.CLOSE
+                    closedDate = now()
+                }
+                log.debug("라이브 스트림 종료 처리 - 채널 ID: {}, 기존 라이브 ID: {}", liveStream.channelId, liveStream.liveId)
+            } else {
+                updateLiveStream(liveStream, fetchLiveDetail)
+                log.debug("라이브 스트림 정보 동기화 완료 - 채널 ID: {}, 라이브 정보: {}", liveStream.channelId, fetchLiveDetail.toString())
+            }
+            
+            liveStreamRepository.save(liveStream)
         }
     }
     
