785f55c74662767350fe5172ee6373011e01e31c
thingk0 <audtjd9080@naver.com>
2025-03-05 23:18:41 +0900

    remove: ParticipantResponseDto 삭제

--- src/main/kotlin/com/chit/app/domain/session/domain/model/ParticipantResponseDto.kt
+++
@@ -1,9 +0,0 @@
-package com.chit.app.domain.session.domain.model
-
-data class ParticipantResponseDto(
-        val viewerId: Long,
-        val chzzkNickname: String,
-        val gameNickname: String,
-        val fixedPick: Boolean,
-        val round: Int
-)
