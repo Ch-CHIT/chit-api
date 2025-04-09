1c007ee4ba84cb1db2d81cf2a0610ece1363464d
thingk0 <audtjd9080@naver.com>
2025-04-09 20:12:37 +0900

    feat: 스프링 캐싱 기능 활성화
    Changes:
    - ChitAppApplication 클래스에 `@EnableCaching` 어노테이션 추가

--- src/main/kotlin/com/chit/app/ChitAppApplication.kt
+++ src/main/kotlin/com/chit/app/ChitAppApplication.kt
@@ -4,6 +4,7 @@ import com.chit.app.domain.auth.infrastructure.properties.JwtFilterProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @EnableConfigurationProperties(JwtFilterProperties::class)
 @EnableScheduling
 @EnableJpaAuditing
+@EnableCaching
 @EnableAsync
 @SpringBootApplication
 class ChitAppApplication
